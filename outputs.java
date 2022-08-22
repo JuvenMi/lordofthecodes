4186df70ec1fc1b6d62b1cd5a806d47c44da1b83
JuvenMi <canercincin34@gmail.com>
2022-08-22 23:32:37 +0300

    Add files via upload

---
+++ outputs.java
@@ -0,0 +1,16 @@
+package post;
+
+public class outputs {
+    public static void main(String[] args) {
+        System.out.print("lord of the codes");
+        System.out.print("takip edip beğenmeyi unutmayın");
+        System.out.print(3+3);
+        System.out.print("lord of the codes" + 1 + "numara");
+
+        System.out.println("Yukardaki fotoğrafta anahtar kelimeler kullanılmamıştır.");
+        System.out.println("Bu fotoğrafta anahtar kelimeler kullanılmıştır.");
+        System.out.print("lord of the codes\t");
+        System.out.print("takip edip beğenmeyi unutmayın\n");
+        System.out.print(3+3);
+    }
+}
